b3c7ff6096ed7168351591890bce56bc9c023ced
Songpei Du <songpeidu@microsoft.com>
2025-04-22 17:30:53 -0500

    Update show.py

--- show.py
+++ show.py
@@ -1,27 +1,19 @@
 from blob import download
-import display.epd12in48b_V2 as eink
 from PIL import Image
 from dotenv import load_dotenv
+from display.display import DisplayHelper
 
 
 load_dotenv()
 
 download()
 
+helper = DisplayHelper(1304, 984)
+
+
 black = Image.open('black-b.png')
 red = Image.open('red-b.png')
 
-epd = eink.EPD()
-height = 984
-width = 1304
+helper.update(black, red)
 
-white = Image.new('1', (width, height), 'white')
-black = Image.new('1', (width, height), 'black')
-
-epd.display(black, white)
-epd.display(white, black)
-epd.display(white, white)
-
-epd.display(black, red)
-
-epd.EPD_Sleep()
+helper.sleep()
