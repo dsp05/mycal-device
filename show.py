f9f5e49e5ef7d4fe92262d80b708d57aeccbb3d4
Songpei Du <songpeidu@microsoft.com>
2025-04-23 13:57:35 -0500

    Update show.py

--- show.py
+++ show.py
@@ -2,7 +2,7 @@ from blob import download
 from PIL import Image
 from dotenv import load_dotenv
 from display.display import DisplayHelper
-
+import os
 
 load_dotenv()
 
@@ -16,3 +16,5 @@ red = Image.open('red-b.png')
 helper.update(black, red)
 
 helper.sleep()
+
+os.system("sudo shutdown -h now")
